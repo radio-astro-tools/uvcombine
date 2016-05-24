721958a7587d4d365c49caef7e1e5f68ba69e108
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-05-24 14:22:15 +0200

    remove 2to3 and do a local import

--- uvcombine/__init__.py
+++ uvcombine/__init__.py
@@ -12,5 +12,5 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from uvcombine import (feather_plot, feather_simple, spectral_regrid,
-                           spectral_smooth_and_downsample)
+    from .uvcombine import (feather_plot, feather_simple, spectral_regrid,
+                            spectral_smooth_and_downsample)
