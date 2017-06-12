bc44c7159d916adbadf757e06023d54c7c0e56e2
e-koch <koch.eric.w@gmail.com>
2017-06-12 16:04:01 -0600

    Add function with multiple methods for finding the scale factor

--- uvcombine/scale_factor.py
+++ uvcombine/scale_factor.py
@@ -19,7 +19,7 @@ import numpy as np
 import FITS_tools
 #from FITS_tools.hcongrid import hcongrid_hdu
 #from FITS_tools.cube_regrid import regrid_cube_hdu
-from astropy import wcs, stats
+from astropy import stats as astrostats
 from astropy.convolution import convolve_fft, Gaussian2DKernel
 from scipy import stats
 
@@ -76,3 +76,115 @@ def find_effSDbeam(hires, lores,
         plt.xlabel("Low Res. FWHM (arcsec)")
 
     return slopes, slopes_CI
+
+
+def find_scale_factor(lowres_pts, highres_pts, method='distrib',
+                      verbose=False,
+                      **method_kwargs):
+    '''
+    Using overlapping points in the uv-plane, find the
+    scale factor. There are three methods implemented:
+
+        * 'distrib' -- fits a Cauchy distribution to the ratio of the points.
+            The center of the distribution is fit, giving the scale factor.
+        * 'linfit' -- fit a robust linear model between the low- and high-res
+            points. This uses `Theil-Sen regression <https://docs.scipy.org/doc/scipy-0.19.0/reference/generated/scipy.stats.theilslopes.html>`_
+            to find the slope, which is the scale factor.
+        * 'clippedstats' -- Uses `~astropy.stats.sigma_clipped_stats` to
+            estimate the scale factor with outlier rejection. Uses the
+            `astropy implementation <http://docs.astropy.org/en/stable/api/astropy.stats.sigma_clipped_stats.html#astropy.stats.sigma_clipped_stats>`_
+
+    Parameters
+    ----------
+    lowres_pts : `~numpy.ndarray` or `~astropy.units.Quantity`
+        Points from the uv-overlap region for the low-resolution data.
+    highres_pts : `~numpy.ndarray` or `~astropy.units.Quantity`
+        Points from the uv-overlap region for the high-resolution data.
+    method : {'distrib', 'linfit', 'clippedstats'}, optional
+        Specify the method to for estimating the scale factor.
+    verbose : bool, optional
+        Enables plotting of the data and the scale factor relation.
+    method_kwargs : Passed to `~scipy.stats.theilslopes` for 'linfit' and
+        `~astropy.stats.sigma_clipped_stats` for 'clippedstats'. Not used by
+        'distrib'.
+
+    Returns
+    -------
+    sc_factor : float
+        The scale factor returned by 'distrib' and 'linfit'.
+    sc_confint : list
+        The confidence interval for the scale factor using 'linfit'.
+    out_dict : dict
+        Returned by 'clippedstats'. Contains the clipped statistic estimates
+        of the mean, median, and standard deviation.
+    '''
+
+    if lowres_pts.size != highres_pts.size:
+        raise ValueError("lowres_pts must be the same size as highres_pts.")
+
+    # Drop the units if Quantities are given.
+    if hasattr(lowres_pts, "value"):
+        lowres_pts = lowres_pts.value
+    if hasattr(highres_pts, "value"):
+        highres_pts = highres_pts.value
+
+    if method == "distrib":
+
+        ratio = highres_pts / lowres_pts
+
+        # Fit a Cauchy distribution to the log of the ratios=
+        log_ratio = np.log(ratio)
+
+        params = stats.cauchy.fit(ratio)
+
+        # The median is the scale factor.
+        sc_factor = np.exp(params[0])
+
+        if verbose:
+            import matplotlib.pyplot as pl
+            pl.hist(log_ratio, bins=int(np.sqrt(lowres_pts.size)), alpha=0.7,
+                    normed=True)
+            x_vals = np.arange(-3, 3, 0.05)
+            pl.plot(x_vals, stats.cauchy.pdf(x_vals, *params))
+            pl.xlabel("log Ratio")
+
+        return sc_factor
+
+    elif method == "linfit":
+
+        sc_factor, intercept, sc_confint = \
+            stats.theilslopes(highres_pts, x=lowres_pts, **method_kwargs)
+
+        if verbose:
+            import matplotlib.pyplot as pl
+            min_fit_pt = lowres_pts.min() * sc_factor + intercept
+            max_fit_pt = lowres_pts.max() * sc_factor + intercept
+            pl.scatter(lowres_pts, highres_pts, alpha=0.7)
+            pl.plot([lowres_pts.min(), lowres_pts.max()],
+                    [min_fit_pt, max_fit_pt])
+
+        return sc_factor, sc_confint
+
+    elif method == "clippedstats":
+
+        ratio = highres_pts / lowres_pts
+
+        sclip = astrostats.sigma_clipped_stats(ratio, **method_kwargs)
+
+        out_dict = {'scale_factor_mean': sclip[0],
+                    'scale_factor_median': sclip[1],
+                    'scale_factor_std': sclip[2]}
+
+        if verbose:
+            import matplotlib.pyplot as pl
+            min_fit_pt = lowres_pts.min() * sclip[1]
+            max_fit_pt = lowres_pts.max() * sclip[1]
+            pl.scatter(lowres_pts, highres_pts, alpha=0.7)
+            pl.plot([lowres_pts.min(), lowres_pts.max()],
+                    [min_fit_pt, max_fit_pt])
+
+        return out_dict
+
+    else:
+        raise ValueError("method must be 'distrib', 'linfit', or "
+                         "'clippedstats'.")
