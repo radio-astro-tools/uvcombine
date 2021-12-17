b8d20263fb8a495677f618a1f263e5f85510bbc8
e-koch <koch.eric.w@gmail.com>
2021-12-17 15:05:43 -0500

    Add basic tests for feathering cubes

--- uvcombine/tests/test_feather.py
+++ uvcombine/tests/test_feather.py
@@ -4,10 +4,10 @@ import astropy.units as u
 from astropy.io import fits
 import numpy.testing as npt
 import numpy as np
-from spectral_cube import Projection
+from spectral_cube import Projection, SpectralCube
 
-from .utils import testing_data
-from ..uvcombine import feather_simple
+from .utils import testing_data, testing_data_cube
+from ..uvcombine import feather_simple, fourier_combine_cubes
 
 
 def test_feather_simple():
@@ -16,13 +16,13 @@ def test_feather_simple():
     orig_hdu, lowres_hdu, highres_hdu = testing_data(return_images=True)
 
     # HDU input
-    combo = feather_simple(lowres_hdu, highres_hdu)
+    combo = feather_simple(highres_hdu, lowres_hdu)
 
     # Projection input
     lowres_proj = Projection.from_hdu(lowres_hdu)
     highres_proj = Projection.from_hdu(highres_hdu)
 
-    combo = feather_simple(lowres_proj, highres_proj)
+    combo = feather_simple(highres_proj, lowres_proj)
 
 
 def test_feather_simple_mismatchunit():
@@ -33,8 +33,35 @@ def test_feather_simple_mismatchunit():
     lowres_hdu.header['BUNIT'] = "K"
 
     with pytest.raises(ValueError):
-        combo = feather_simple(lowres_hdu, highres_hdu, match_units=False)
+        combo = feather_simple(highres_hdu, lowres_hdu, match_units=False)
 
 
 def test_feather_simple_cube():
-    pass
+
+    orig_cube, sd_cube, interf_cube = testing_data_cube()
+
+    combo_cube = fourier_combine_cubes(interf_cube, sd_cube, return_hdu=True)
+
+    combo_cube_sc = SpectralCube.read(combo_cube)
+
+    assert orig_cube.shape == combo_cube_sc.shape
+
+    assert combo_cube_sc.unit == interf_cube.unit
+
+
+def test_feather_simple_cube_diffunits():
+
+    orig_cube, sd_cube, interf_cube = testing_data_cube()
+
+    interf_cube = interf_cube.to(u.Jy / u.beam)
+
+    combo_cube = fourier_combine_cubes(interf_cube, sd_cube, return_hdu=True)
+
+    combo_cube_sc = SpectralCube.read(combo_cube)
+
+    assert orig_cube.shape == combo_cube_sc.shape
+
+    # Output units should in the units of the interferometer cube
+    print(combo_cube_sc.unit, interf_cube.unit)
+
+    assert combo_cube_sc.unit == interf_cube.unit
