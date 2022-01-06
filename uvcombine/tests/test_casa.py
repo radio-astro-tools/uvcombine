5bd121a6e30d990fb3e7420ed8ba3f0640ea5098
e-koch <koch.eric.w@gmail.com>
2022-01-06 17:48:18 -0500

    Revamp feather test against casa; it runs but it fails...

--- uvcombine/tests/test_casa.py
+++ uvcombine/tests/test_casa.py
@@ -2,61 +2,69 @@ import numpy as np
 import pytest
 import itertools
 from astropy.io import fits
+from spectral_cube import SpectralCube, Projection
+
 try:
-    import casa
-    from taskinit import ia
-except ImportError:
-    from casatasks import feather
+    from casatasks import feather, exportfits, importfits
+    from casatools import image
+
+    ia = image()
+
+    casa_imported = True
+
 except ImportError:
     casa_imported = False
 
-from . import path
-from .. import uvcombine
+from .. import feather_simple
 
 @pytest.mark.skipif('not casa_imported')
 @pytest.mark.parametrize(('sdfactor', 'lowpassfilterSD'), itertools.product((0.5, 1, 1.5), (True,False)))
-def test_casafeather(sdfactor, lowpassfilterSD):
-    try:
-        from casa import feather,exportfits,importfits
-    except ImportError:
-        from casatasks import feather,exportfits,importfits
+def test_casafeather(image_sz512as_pl1p5_fwhm2as_scale1as, sdfactor, lowpassfilterSD):
 
-    print(f"Testing casafeather with sdfactor={sdfactor}")
+    tmp_path, input_fn, intf_fn, sd_fn = image_sz512as_pl1p5_fwhm2as_scale1as
 
-    input_hires = path('input_image_sz512as_pl1.5_fwhm2as_scale1as_intf2to40as.fits')
-    input_lores = path('input_image_sz512as_pl1.5_fwhm2as_scale1as_sd33as.fits')
+    intf_hdu = fits.open(intf_fn)[0]
+    sd_hdu = fits.open(sd_fn)[0]
 
-    importfits(fitsimage=input_hires, imagename=input_hires.replace(".fits",".image"), overwrite=True)
-    importfits(fitsimage=input_lores, imagename=input_lores.replace(".fits",".image"), overwrite=True)
-    output = path(f'casafeathered_sz512as_pl1.5_fwhm2as_scale1as_intf2to40as_w_sd33as_sdf={sdfactor}_lowpass={lowpassfilterSD}.image')
+    # Feathering with CASA
 
-    feather(imagename=output,
-            highres=input_hires.replace(".fits",".image"),
-            lowres=input_lores.replace(".fits",".image"),
+    intf_fn_image = intf_fn.parent / intf_fn.name.replace(".fits",".image")
+    sd_fn_image = sd_fn.parent / sd_fn.name.replace(".fits",".image")
+
+    # CASA needs a posix string to work
+    importfits(fitsimage=intf_fn.as_posix(), imagename=intf_fn_image.as_posix())
+    importfits(fitsimage=sd_fn.as_posix(), imagename=sd_fn_image.as_posix())
+
+    output_name = tmp_path / 'casafeathered.image'
+
+    feather(imagename=output_name.as_posix(),
+            highres=intf_fn_image.as_posix(),
+            lowres=sd_fn_image.as_posix(),
             sdfactor=sdfactor,
             lowpassfiltersd=lowpassfilterSD,
            )
-    exportfits(imagename=output, fitsimage=output+".fits",
-               overwrite=True, dropdeg=True)
 
-    feathered_hdu = uvcombine.feather_simple(hires=input_hires,
-                                             lores=input_lores,
-                                             lowresscalefactor=sdfactor,
-                                             lowpassfilterSD=lowpassfilterSD,
-                                             return_hdu=True)
-    output2 = path(f'uvcombinefeathered_sz512as_pl1.5_fwhm2as_scale1as_intf2to40as_w_sd33as_sdf={sdfactor}_lowpass={lowpassfilterSD}.fits')
-    feathered_hdu.writeto(output2, overwrite=True)
+    # Right now read as spectralcube despite being a 2D image.
+    casa_feather_proj = SpectralCube.read(output_name)[0]
 
-    diff = fits.getdata(output+".fits") - feathered_hdu.data
-    newhdu = feathered_hdu.copy()
-    newhdu.data = diff
-    output_diff = path(f'casafeathered-uvcombinefeathered_sz512as_pl1.5_fwhm2as_scale1as_intf2to40as_w_sd33as_sdf={sdfactor}_lowpass={lowpassfilterSD}.fits')
-    newhdu.writeto(output_diff, overwrite=True)
+    # exportfits(imagename=output, fitsimage=output+".fits",
+    #            overwrite=True, dropdeg=True)
+
+    # Feathering with uvcombine
+    feathered_hdu = feather_simple(hires=intf_hdu, lores=sd_hdu,
+                                   lowresscalefactor=sdfactor,
+                                   lowpassfilterSD=lowpassfilterSD,
+                                   return_hdu=True)
+
+    uvcomb_feather_proj = Projection.from_hdu(feathered_hdu)
+
+    diff = (casa_feather_proj - uvcomb_feather_proj).value
 
     print("Proof that we have exactly reimplemented CASA's feather: ")
     print("((casa-uvcombine)**2 / casa**2).sum() = {0}"
-          .format(((diff**2)/(fits.getdata(output+".fits")**2)).sum()))
+          .format(((diff**2)/(casa_feather_proj.value**2)).sum()))
     print("Maximum of abs(diff): {0}".format(np.abs(diff).max()))
+
     if lowpassfilterSD:
         assert np.abs(diff).max() < 2e-4
     else:
