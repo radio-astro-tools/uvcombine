6d7f2fa789010165ba10c5153253a7883dde4a3d
e-koch <koch.eric.w@gmail.com>
2022-01-06 22:03:56 -0500

    This issues are coming from the new make_extended I swapped in in #28...

--- uvcombine/utils.py
+++ uvcombine/utils.py
@@ -11,106 +11,133 @@ from radio_beam import Beam
 from .uvcombine import feather_compare
 
 
-def make_extended(imsize, powerlaw=2.0,
-                  return_fft=False, full_fft=True, seed=32788324):
-    '''
+# def make_extended(imsize, powerlaw=2.0,
+#                   return_fft=False, full_fft=True, seed=32788324):
+#     '''
 
-    Generate a 2D power-law image with a specified index and random phases.
+#     Generate a 2D power-law image with a specified index and random phases.
 
-    Adapted from https://github.com/keflavich/image_registration and
-    https://github.com/Astroua/TurbuStat/blob/master/turbustat/simulator/gen_field.py.
+#     Adapted from https://github.com/keflavich/image_registration and
+#     https://github.com/Astroua/TurbuStat/blob/master/turbustat/simulator/gen_field.py.
 
-    Parameters
-    ----------
-    imsize : int
-        Array size.
-    powerlaw : float, optional
-        Powerlaw index.
-    return_fft : bool, optional
-        Return the FFT instead of the image. The full FFT is
-        returned, including the redundant negative phase phases for the RFFT.
-    full_fft : bool, optional
-        When `return_fft=True`, the full FFT, with negative frequencies, will
-        be returned. If `full_fft=False`, the RFFT is returned.
-    randomseed: int, optional
-        Seed for random number generator.
+#     Parameters
+#     ----------
+#     imsize : int
+#         Array size.
+#     powerlaw : float, optional
+#         Powerlaw index.
+#     return_fft : bool, optional
+#         Return the FFT instead of the image. The full FFT is
+#         returned, including the redundant negative phase phases for the RFFT.
+#     full_fft : bool, optional
+#         When `return_fft=True`, the full FFT, with negative frequencies, will
+#         be returned. If `full_fft=False`, the RFFT is returned.
+#     randomseed: int, optional
+#         Seed for random number generator.
 
-    Returns
-    -------
-    newmap : np.ndarray
-        Two-dimensional array with the given power-law properties.
-    full_powermap : np.ndarray
-        The 2D array in Fourier space. The zero-frequency is shifted to
-        the centre.
-    '''
+#     Returns
+#     -------
+#     newmap : np.ndarray
+#         Two-dimensional array with the given power-law properties.
+#     full_powermap : np.ndarray
+#         The 2D array in Fourier space. The zero-frequency is shifted to
+#         the centre.
+#     '''
+#     imsize = int(imsize)
+
+#     yy, xx = np.meshgrid(np.fft.fftfreq(imsize),
+#                          np.fft.rfftfreq(imsize), indexing="ij")
+
+#     # Circular whenever ellip == 1
+#     rr = (xx**2 + yy**2)**0.5
+
+#     # flag out the bad point to avoid warnings
+#     rr[rr == 0] = np.nan
+
+#     with NumpyRNGContext(seed):
+
+#         Np1 = (imsize - 1) // 2 if imsize % 2 != 0 else imsize // 2
+
+#         angles = np.random.uniform(0, 2 * np.pi,
+#                                    size=(imsize, Np1 + 1))
+
+#     phases = np.cos(angles) + 1j * np.sin(angles)
+
+#     # Rescale phases to an amplitude of unity
+#     phases /= np.sqrt(np.sum(phases**2) / float(phases.size))
+
+#     output = (rr**(-powerlaw / 2.)).astype('complex') * phases
+
+#     output[np.isnan(output)] = 0.
+
+#     # Impose symmetry
+#     # From https://dsp.stackexchange.com/questions/26312/numpys-real-fft-rfft-losing-power
+#     if imsize % 2 == 0:
+#         output[1:Np1, 0] = np.conj(output[imsize:Np1:-1, 0])
+#         output[1:Np1, -1] = np.conj(output[imsize:Np1:-1, -1])
+#         output[Np1, 0] = output[Np1, 0].real + 1j * 0.0
+#         output[Np1, -1] = output[Np1, -1].real + 1j * 0.0
+
+#     else:
+#         output[1:Np1 + 1, 0] = np.conj(output[imsize:Np1:-1, 0])
+#         output[1:Np1 + 1, -1] = np.conj(output[imsize:Np1:-1, -1])
+
+#     # Zero freq components must have no imaginary part to be own conjugate
+#     output[0, -1] = output[0, -1].real + 1j * 0.0
+#     output[0, 0] = output[0, 0].real + 1j * 0.0
+
+#     if return_fft:
+
+#         if not full_fft:
+#             return output
+
+#         # Create the full power map, with the symmetric conjugate component
+#         if imsize % 2 == 0:
+#             power_map_symm = np.conj(output[:, -2:0:-1])
+#         else:
+#             power_map_symm = np.conj(output[:, -1:0:-1])
+
+#         power_map_symm[1::, :] = power_map_symm[:0:-1, :]
+
+#         full_powermap = np.concatenate((output, power_map_symm), axis=1)
+
+#         if not full_powermap.shape[1] == imsize:
+#             raise ValueError("The full output should have a square shape."
+#                              " Instead has {}".format(full_powermap.shape))
+
+#         return np.fft.fftshift(full_powermap)
+
+#     newmap = np.fft.irfft2(output)
+
+#     # amp = 10.
+#     # newmap /= (np.sqrt(np.sum(newmap**2)) / np.sqrt(newmap.size)) / amp
+
+#     # newmap += 5 * newmap.std()
+#     # newmap[newmap < 0.0] = 0.
+
+#     return newmap
+
+def make_extended(imsize, powerlaw=2.0, seed=0):
     imsize = int(imsize)
+    imsize2=imsize
 
-    yy, xx = np.meshgrid(np.fft.fftfreq(imsize),
-                         np.fft.rfftfreq(imsize), indexing="ij")
-
-    # Circular whenever ellip == 1
-    rr = (xx**2 + yy**2)**0.5
+    yy,xx = np.indices((imsize2,imsize), dtype='float')
+    xcen = imsize/2.-(1.-imsize % 2)
+    ycen = imsize2/2.-(1.-imsize2 % 2)
+    yy -= ycen
+    xx -= xcen
+    rr = (xx**2+yy**2)**0.5
 
     # flag out the bad point to avoid warnings
     rr[rr == 0] = np.nan
 
-    with NumpyRNGContext(seed):
+    powermap = (np.random.randn(imsize2, imsize) * rr**(-powerlaw)+
+                np.random.randn(imsize2, imsize) * rr**(-powerlaw) * 1j)
+    powermap[powermap!=powermap] = 0
 
-        Np1 = (imsize - 1) // 2 if imsize % 2 != 0 else imsize // 2
-
-        angles = np.random.uniform(0, 2 * np.pi,
-                                   size=(imsize, Np1 + 1))
-
-    phases = np.cos(angles) + 1j * np.sin(angles)
-
-    # Rescale phases to an amplitude of unity
-    phases /= np.sqrt(np.sum(phases**2) / float(phases.size))
-
-    output = (rr**(-powerlaw / 2.)).astype('complex') * phases
-
-    output[np.isnan(output)] = 0.
-
-    # Impose symmetry
-    # From https://dsp.stackexchange.com/questions/26312/numpys-real-fft-rfft-losing-power
-    if imsize % 2 == 0:
-        output[1:Np1, 0] = np.conj(output[imsize:Np1:-1, 0])
-        output[1:Np1, -1] = np.conj(output[imsize:Np1:-1, -1])
-        output[Np1, 0] = output[Np1, 0].real + 1j * 0.0
-        output[Np1, -1] = output[Np1, -1].real + 1j * 0.0
-
-    else:
-        output[1:Np1 + 1, 0] = np.conj(output[imsize:Np1:-1, 0])
-        output[1:Np1 + 1, -1] = np.conj(output[imsize:Np1:-1, -1])
-
-    # Zero freq components must have no imaginary part to be own conjugate
-    output[0, -1] = output[0, -1].real + 1j * 0.0
-    output[0, 0] = output[0, 0].real + 1j * 0.0
-
-    if return_fft:
-
-        if not full_fft:
-            return output
-
-        # Create the full power map, with the symmetric conjugate component
-        if imsize % 2 == 0:
-            power_map_symm = np.conj(output[:, -2:0:-1])
-        else:
-            power_map_symm = np.conj(output[:, -1:0:-1])
-
-        power_map_symm[1::, :] = power_map_symm[:0:-1, :]
-
-        full_powermap = np.concatenate((output, power_map_symm), axis=1)
-
-        if not full_powermap.shape[1] == imsize:
-            raise ValueError("The full output should have a square shape."
-                             " Instead has {}".format(full_powermap.shape))
-
-        return np.fft.fftshift(full_powermap)
-
-    newmap = np.fft.irfft2(output)
+    newmap = np.abs(np.fft.fftshift(np.fft.fft2(powermap)))
 
     return newmap
-
 
 def interferometrically_observe_image(image, pixel_scale,
                                       largest_angular_scale,
