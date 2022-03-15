4a4396c3c3527c4968c01f4b15872f3d813e40e3
e-koch <koch.eric.w@gmail.com>
2022-03-15 15:21:27 -0400

    Re-add feather_simple_cube (my fault on merging conflicts in the rebase)

--- uvcombine/uvcombine.py
+++ uvcombine/uvcombine.py
@@ -972,6 +972,37 @@ def spectral_smooth_and_downsample(cube, kernelfwhm):
     return cube_ds_hdu
 
 
+def feather_simple_cube(hires, lores, **kwargs):
+    """
+    Parameters
+    ----------
+    hires : cube
+    lores : cube
+    TODO:
+      * Can we paralellize, daskify, or otherwise not-in-memory-ify this?
+    """
+
+    if not hasattr(hires, shape):
+        hires = SpectralCube.read(hires)
+    if not hasattr(hires, shape):
+        lores = SpectralCube.read(lores)
+
+    if lores.shape[0]!=hires.shape[0] or not all(lores.spectral_axis == hires.spectral_axis):
+        lores = lores.spectral_interpolate(hires.spectral_axis)
+
+    feathcube = []
+    pb = ProgressBar(len(hires))
+    for hslc, lslc in zip(hires, lores):
+        feath = feather_simple(hslc.hdu, lslc.hdu)
+        feathcube.append(feath)
+        pb.update()
+
+    newcube = SpectralCube(data=np.array(feathcube).real, header=hires.header,
+            wcs=hires.wcs)
+
+    return newcube
+
+
 def fourier_combine_cubes(cube_hi, cube_lo,
                           highresscalefactor=1.0,
                           lowresscalefactor=1.0, lowresfwhm=1*u.arcmin,
